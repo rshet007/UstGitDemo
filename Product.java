28e20285135ba251e7e7e8c2a73958cc773c3812
Syed Irfan <isyed287@gmail.com>
2024-12-26 21:44:13 +0530

    Modified Product and test

---
+++ Product.java
@@ -0,0 +1,6 @@
+public class product
+{
+    public static void main(String[]args){
+        System.out.println("Hello world");
+    }
+}
--- test.java
+++ test.java
@@ -1,5 +1,10 @@
 class test{
     public static void main(String[]args){
-        System.out.println("Hello World")
+        System.out.println("Hello World");
+
+    }
+
+    public static void sayHello(){
+        System.out.println("Haii Friends!");
     }
 }
