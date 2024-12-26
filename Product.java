2f9f429a6e74057172d0a3705b55966729817b06
rshet007 <rshet007@yahoo.com>
2024-12-26 21:41:48 +0530

    Modified test.java and added Product.java

---
+++ Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    public static void main(String[] args) {
+        int a=25;
+        int b=12;
+        int c = a*b;
+        System.out.println("Product : "+c);
+    }
+    
+}
