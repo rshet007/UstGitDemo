ae8aeb607eac2fe719f7cf2fdefe84cbe5a02237
JatinMeh <Jatin3105@gmail.com>
2024-12-26 21:44:15 +0530

    Updated test.java

--- test.java
+++ test.java
@@ -1,5 +1,9 @@
 class test{
     public static void main(String[]args){
-        System.out.println("Hello World")
+        System.out.println("Hello World");
+    }
+
+    public static void sayHello(){
+        System.out.println("Bye");
     }
 }
